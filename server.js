9cda94999ecc7fd3d40e5a8622063953772170fd
Luke Priebe <lucas_priebe@brown.edu>
2017-04-14 10:27:48 -0400

    again?

--- server.js
+++ server.js
@@ -88,7 +88,6 @@ function setup()
 
    // anything below here requires login
    app.use(sessionmanager.isLoggedInMiddleware);
-   app.use("/benefits", sessionmanager.isAdminUserMiddleware);
 
    // The main page of the app
    app.get("/dashboard", sessionmanager.displayWelcomePage);
@@ -102,8 +101,8 @@ function setup()
    app.post("/contributions", contributions.handleContributionsUpdate);
 
    // Benefits Page
-   app.get("/benefits", benefits.displayBenefits);
-   app.post("/benefits", benefits.updateBenefits);
+   app.get("/benefits", isAdmin, benefits.displayBenefits);
+   app.post("/benefits", isAdmin, benefits.updateBenefits);
 
    // Allocations Page
    app.get("/allocations/:userId", allocations.displayAllocations);
